261fb41e1b7bce14e938f9985090cbf8626710d9
Raj <rajsharma222547@gmail.com>
2025-12-23 00:06:23 +0530

    build: Create the Dzin-AI

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+function AppLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
+
+export default AppLayout;
